1781760f66ac2c09e6f0a8347020f72758bbe06f
emartinez004 <eudes.martinez@etu.u-bordeaux.fr>
2018-09-26 15:04:42 +0200

    Proper indetation synchconsole

--- code/userprog/synchconsole.cc
+++ code/userprog/synchconsole.cc
@@ -3,35 +3,24 @@
 #include "system.h"
 #include "synchconsole.h"
 #include "synch.h"
-static Semaphore
-*
-readAvail;
-static Semaphore
-*
-writeDone;
-static void ReadAvailHandler(void
-*
-arg) { (void) arg; readAvail->V(); }
-static void WriteDoneHandler(void
-*
-arg) { (void) arg; writeDone->V(); }
-SynchConsole::SynchConsole(const char
-*
-in, const char
-*
-out)
-{
-readAvail = new Semaphore("read avail", 0);
-writeDone = new Semaphore("write done", 0);
-console = ...
+
+static Semaphore *readAvail;
+static Semaphore *writeDone;
+static void ReadAvailHandler(void *arg) { (void) arg; readAvail->V(); }
+static void WriteDoneHandler(void *arg) { (void) arg; writeDone->V(); }
+
+SynchConsole::SynchConsole(const char *in, const char *out) {
+	readAvail = new Semaphore("read avail", 0);
+	writeDone = new Semaphore("write done", 0);
+	console = ...
 }
-SynchConsole::~SynchConsole()
-{
-delete console;
-delete writeDone;
-delete readAvail;
+
+SynchConsole::~SynchConsole() {
+	delete console;
+	delete writeDone;
+	delete readAvail;
 }
-void SynchConsole::SynchPutChar(int ch)
-{
+
+void SynchConsole::SynchPutChar(int ch) {
 // ...
 }
