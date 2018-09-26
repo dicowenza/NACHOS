55ccbdad371cb6ee887473cadb56e060f809b2b1
Noé <emmanoe973@gmail.com>
2018-09-26 18:09:55 +0200

    #define SC_PutChar --> syscall.h

--- code/userprog/syscall.h
+++ code/userprog/syscall.h
@@ -1,12 +1,12 @@
-/* syscalls.h 
+/* syscalls.h
  * 	Nachos system call interface.  These are Nachos kernel operations
  * 	that can be invoked from user programs, by trapping to the kernel
  *	via the "syscall" instruction.
  *
- *	This file is included by user programs and by the Nachos kernel. 
+ *	This file is included by user programs and by the Nachos kernel.
  *
  * Copyright (c) 1992-1993 The Regents of the University of California.
- * All rights reserved.  See copyright.h for copyright notice and limitation 
+ * All rights reserved.  See copyright.h for copyright notice and limitation
  * of liability and disclaimer of warranty provisions.
  */
 
@@ -31,6 +31,7 @@
 #define SC_Close	8
 #define SC_Fork		9
 #define SC_Yield	10
+#define SC_PutChar	11
 
 #ifdef IN_USER_MODE
 
@@ -41,10 +42,10 @@
 /* The system call interface.  These are the operations the Nachos
  * kernel needs to support, to be able to run user programs.
  *
- * Each of these is invoked by a user program by simply calling the 
+ * Each of these is invoked by a user program by simply calling the
  * procedure; an assembly language stub stuffs the system call code
  * into a register, and traps to the kernel.  The kernel procedures
- * are then invoked in the Nachos kernel, after appropriate error checking, 
+ * are then invoked in the Nachos kernel, after appropriate error checking,
  * from the system call entry point in exception.cc.
  */
 
@@ -60,12 +61,12 @@ void Exit (int status) __attribute__ ((__noreturn__));
 /* A unique identifier for an executing user program (address space) */
 typedef int SpaceId;
 
-/* Run the executable, stored in the Nachos file "name", and return the 
+/* Run the executable, stored in the Nachos file "name", and return the
  * address space identifier
  */
 SpaceId Exec (const char *name);
 
-/* Only return once the the user program "id" has finished.  
+/* Only return once the the user program "id" has finished.
  * Return the exit status.
  */
 int Join (SpaceId id);
@@ -83,7 +84,7 @@ int Join (SpaceId id);
 /* A unique identifier for an open Nachos file. */
 typedef int OpenFileId;
 
-/* when an address space starts up, it has two open files, representing 
+/* when an address space starts up, it has two open files, representing
  * keyboard input and display output (in UNIX terms, stdin and stdout).
  * Read and Write can be used directly on these, without first opening
  * the console device.
@@ -95,7 +96,7 @@ typedef int OpenFileId;
 /* Create a Nachos file, with "name" */
 void Create (const char *name);
 
-/* Open the Nachos file "name", and return an "OpenFileId" that can 
+/* Open the Nachos file "name", and return an "OpenFileId" that can
  * be used to read and write to the file.
  */
 OpenFileId Open (const char *name);
@@ -103,10 +104,10 @@ OpenFileId Open (const char *name);
 /* Write "size" bytes from "buffer" to the open file. */
 void Write (const void *buffer, int size, OpenFileId id);
 
-/* Read "size" bytes from the open file into "buffer".  
+/* Read "size" bytes from the open file into "buffer".
  * Return the number of bytes actually read -- if the open file isn't
- * long enough, or if it is an I/O device, and there aren't enough 
- * characters to read, return whatever is available (for I/O devices, 
+ * long enough, or if it is an I/O device, and there aren't enough
+ * characters to read, return whatever is available (for I/O devices,
  * you should always wait until you can return at least one character).
  */
 int Read (void *buffer, int size, OpenFileId id);
@@ -117,18 +118,21 @@ void Close (OpenFileId id);
 
 
 /* User-level thread operations: Fork and Yield.  To allow multiple
- * threads to run within a user program. 
+ * threads to run within a user program.
  */
 
-/* Fork a thread to run a procedure ("func") in the *same* address space 
+/* Fork a thread to run a procedure ("func") in the *same* address space
  * as the current thread.
  */
 void Fork (void (*func) ());
 
-/* Yield the CPU to another runnable thread, whether in this address space 
- * or not. 
+/* Yield the CPU to another runnable thread, whether in this address space
+ * or not.
  */
 void Yield ();
+
+/* output of characters and strings */
+void PutChar(char c);
 
 #endif // IN_USER_MODE
 
