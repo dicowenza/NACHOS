2437f95316343177789ae3967ec1565a99f6860d
Noé <emmanoe973@gmail.com>
2018-10-13 13:13:31 +0200

    implementation des tests

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -1,5 +1,3 @@
-#ifdef CHANGED
-
 #include "syscall.h"
 
 void print(char c, int n) {
@@ -8,32 +6,9 @@ void print(char c, int n) {
 	for (i = 0; i < n; i++)
 		PutChar(c + i);
 	PutChar('\n');
-	// ----- PutString
-	PutString("Bonjour\n");
-}
-
-void get() {
-	int c;
-	char str[100];
-
-	// ----- GetChar
-	PutString("Saisir un char : ");
-	c = GetChar();
-	PutString("Le char saisit est : ");
-	PutChar((char)c);
-
-	// ----- GetString
-	PutString("\nSaisir une str (10 char max.) : ");
-	GetString(str, 10);
-	PutString("La str saisit est : ");
-	PutString(str);
 }
 
 int main() {
 	print('a', 4);
-	get();
-	// Exit(1);
-	return 0;
+	Exit(0);
 }
-
-#endif
--- code/test/putstring.c
+++ code/test/putstring.c
@@ -3,16 +3,8 @@
 #include "syscall.h"
 
 int main() {
-    char str[100];
-    PutString("1. Get : ");
-    GetString(str, 29);
-    PutString("\n2. Put : ");
-    PutString(str);
-    PutString("\n");
-    
-    // Test putstring alphabet (MAX_STRING_SIZE = 10)
-    // PutString("abcdefghijklmnopqrstuvwxyz\n");
-    return 0;
+	PutString("abcdefghijklmnopqrstuvwxyz\n");
+	Exit(0);
 }
 
 #endif
