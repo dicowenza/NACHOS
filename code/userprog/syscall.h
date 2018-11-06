34c5ccb5c0eb7c843cff88a8cf8b3cb6f4b5cbef
Eudes Martinez <eudes.martinez@etu.u-bordeaux.fr>
2018-11-06 17:18:30 +0100

    Setup syscall interface

--- code/userprog/syscall.h
+++ code/userprog/syscall.h
@@ -38,6 +38,9 @@
 #define SC_GetString 14
 #define SC_PutInt   15
 #define SC_GetInt   16
+
+#define SC_ThreadCreate 17
+#define SC_ThreadExit 18
 #endif
 
 #ifdef IN_USER_MODE
@@ -145,6 +148,9 @@ void PutString(const char s[]);
 void GetString(char *s, int n);
 void PutInt(int n);
 int GetInt();
+
+int ThreadCreate(void f(void *arg), void *arg);
+void ThreadExit(void);
 #endif
 
 #endif // IN_USER_MODE
