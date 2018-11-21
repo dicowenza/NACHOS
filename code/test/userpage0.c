e1a76448522234be0ae77ed7205fbd66e24cc733
Eudes Martinez <eudes.martinez@etu.u-bordeaux.fr>
2018-11-21 18:26:15 +0100

    ReadAtVirtual Implementation

---
+++ code/test/userpage0.c
@@ -0,0 +1,10 @@
+#ifdef CHANGED
+
+#include "syscall.h"
+
+int main() {
+	PutString("Hello !\n");
+	return 0;
+}
+
+#endif
--- code/userprog/addrspace.cc
+++ code/userprog/addrspace.cc
@@ -26,6 +26,30 @@
 #endif
 #include "new"
 
+#ifdef CHANGED 
+static void ReadAtVirtual(OpenFile *executable, int virtualaddr, int numBytes, int position,
+TranslationEntry *pageTable, unsigned int numPages) {
+
+    char *buffer = new char[numBytes];
+    int nb_read_bytes = executable->ReadAt(buffer, numBytes, position);
+
+    TranslationEntry *saved_pageTable = machine->pageTable;
+    int saved_pageTableSize = machine->pageTableSize;
+
+    machine->pageTable = pageTable;
+    machine->pageTableSize = numPages;
+
+    for (int i = 0; i < nb_read_bytes; i++) {
+        machine->WriteMem(virtualaddr + i, 1, buffer[i]);
+    }
+
+    machine->pageTable = saved_pageTable;
+    machine->pageTableSize = saved_pageTableSize;
+
+}
+#endif
+
+
 //----------------------------------------------------------------------
 // SwapHeader
 //      Do little endian to big endian conversion on the bytes in the
@@ -114,19 +138,18 @@ AddrSpace::AddrSpace (OpenFile * executable)
 // then, copy in the code and data segments into memory
     if (noffH.code.size > 0)
       {
-	  DEBUG ('a', "Initializing code segment, at 0x%x, size 0x%x\n",
-		 noffH.code.virtualAddr, noffH.code.size);
-	  executable->ReadAt (&(machine->mainMemory[noffH.code.virtualAddr]),
-			      noffH.code.size, noffH.code.inFileAddr);
+	    DEBUG ('a', "Initializing code segment, at 0x%x, size 0x%x\n",
+		    noffH.code.virtualAddr, noffH.code.size);
+	    ReadAtVirtual(executable, noffH.code.virtualAddr, 
+            noffH.code.size, noffH.code.inFileAddr, pageTable, numPages);
+
       }
     if (noffH.initData.size > 0)
       {
-	  DEBUG ('a', "Initializing data segment, at 0x%x, size 0x%x\n",
-		 noffH.initData.virtualAddr, noffH.initData.size);
-	  executable->ReadAt (&
-			      (machine->mainMemory
-			       [noffH.initData.virtualAddr]),
-			      noffH.initData.size, noffH.initData.inFileAddr);
+	    DEBUG ('a', "Initializing data segment, at 0x%x, size 0x%x\n",
+		    noffH.initData.virtualAddr, noffH.initData.size);
+        ReadAtVirtual(executable, noffH.initData.virtualAddr, 
+            noffH.initData.size, noffH.initData.inFileAddr, pageTable, numPages);
       }
 
     DEBUG ('a', "Area for stacks at 0x%x, size 0x%x\n",
